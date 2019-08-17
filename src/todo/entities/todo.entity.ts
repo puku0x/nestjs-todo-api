2ec66c7ebfc73639955056843c245d4ad86cd0d3
puku0x <puku0x@gmail.com>
2019-08-17 21:06:55 +0900

    set default value

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -14,8 +14,8 @@ export class TodoEntity {
   @Column({ length: 255 })
   text: string;
 
-  @Column({ type: 'boolean' })
-  checked = false;
+  @Column({ default: false })
+  checked: boolean;
 
   @CreateDateColumn({ type: 'timestamp' })
   readonly createdAt?: Date;
